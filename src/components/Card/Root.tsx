c85985b8ed1c5cda68aafeca435877cd792583d7
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-12-01 11:22:05 -0300

    feat: adiciona animacao no Card

--- src/components/Card/Root.tsx
+++ src/components/Card/Root.tsx
@@ -2,9 +2,8 @@ import { ComponentProps } from 'react'
 import { ClassNameValue, twMerge } from 'tailwind-merge'
 
 const base =
-  'col-span-1 row-span-1 flex w-full flex-col gap-4 rounded outline-none md:gap-6' as ClassNameValue
-const dark =
-  'dark:focus-within:ring-2 dark:focus-within:ring-indigo-400' as ClassNameValue
+  'col-span-1 row-span-1 flex w-full flex-col gap-4 rounded outline-none focus-within:ring-2 focus-within:ring-indigo-400 md:gap-6' as ClassNameValue
+const dark = ' dark:focus-within:ring-blue-400' as ClassNameValue
 
 export type RootProps = ComponentProps<'a'>
 
